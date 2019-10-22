5e113f29da6696059a17518ecfc32664dd81247c
Jismy Sebastian <jismy.sebastian@ust-global.com>
2019-10-22 20:35:58 +0530

    issue fixed need to add test

--- mysql_flask/app/models.py
+++ mysql_flask/app/models.py
@@ -1,7 +1,11 @@
 from app import db
 
 class Entry(db.Model):
+    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True, nullable=False)
     description = db.Column(db.String(120), index=True, nullable=False)
     status = db.Column(db.Boolean, default=False)
+
+db.create_all()
+db.session.commit()
